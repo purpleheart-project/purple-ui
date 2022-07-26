9459a0c0c0b72682727830cc5cbe33e664dabed8
zhangtao25 <tzhangm@trip.com>
2022-07-26 21:07:47 +0800

    upd

--- src/components/collection/index.tsx
+++ src/components/collection/index.tsx
@@ -1,54 +1,91 @@
 import { DownOutlined } from '@ant-design/icons';
-import { Tree } from 'antd';
+import { Button, Tree } from 'antd';
 import { DataNode } from 'antd/lib/tree';
 import CollectionTitleRender from './CollectionTitleRender';
-import {useMount} from "react-use";
-import {CollectionService} from "../../services/CollectionService";
+import { useMount } from 'react-use';
+import { CollectionService } from '../../services/CollectionService';
+import { useEffect, useState } from 'react';
 
 const Collection = () => {
-  function onExpand() {}
-  function onSelect() {}
+  const [expandedKeys, setExpandedKeys] = useState<React.Key[]>(['0-0-2']);
+  const [selectedKeys, setSelectedKeys] = useState<React.Key[]>([]);
+  const [autoExpandParent, setAutoExpandParent] = useState<boolean>(true);
+  const [treeData, setTreeData] = useState([]);
 
-  const treeData: DataNode[] = [
-    {
-      title: 'parent 1',
-      key: '0-0',
-      children: [
-        {
-          title: 'parent 1-0',
-          key: '0-0-0',
-          disabled: true,
-          children: [
-            {
-              title: 'leaf',
-              key: '0-0-0-0',
-              disableCheckbox: true,
-            },
-            {
-              title: 'leaf',
-              key: '0-0-0-1',
-            },
-          ],
-        }
-      ],
-    },
-  ];
+  useMount(() => {
+    CollectionService.qcollection().then((res) => {
+      setTreeData(res.data);
+    });
+  });
 
-  useMount(()=>{
-    CollectionService.qcollection().then(res=>{
-      console.log(res,'res')
-    })
-  })
+  const onExpand = (expandedKeysValue: React.Key[]) => {
+    console.log('onExpand', expandedKeysValue);
+    // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+    // or, you can remove all expanded children keys.
+    setExpandedKeys(expandedKeysValue);
+    setAutoExpandParent(false);
+  };
+
+  const onSelect = (selectedKeysValue: React.Key[], info: any) => {
+    console.log('onSelect', info);
+    setSelectedKeys(selectedKeysValue);
+  };
+
   return (
-    <div className={'collection'}>
+    <div>
       <Tree
-        blockNode={true}
         onExpand={onExpand}
+        expandedKeys={expandedKeys}
+        autoExpandParent={autoExpandParent}
         onSelect={onSelect}
-        switcherIcon={<DownOutlined />}
+        selectedKeys={selectedKeys}
         treeData={treeData}
-        titleRender={(val) => <CollectionTitleRender />}
       />
+      <Button
+        onClick={() => {
+          CollectionService.qcollection().then((res) => {
+            setTreeData([
+              {
+                title: 'parent 1',
+                key: '0-0',
+                children: [
+                  {
+                    title: 'parent 1-0',
+                    key: '0-0-0',
+                    children: [
+                      {
+                        title: 'leaf1',
+                        key: '0-0-0-0',
+                      },
+                      {
+                        title: 'leaf2',
+                        key: '0-0-0-1',
+                      },
+                    ],
+                  },
+                ],
+              },
+              {
+                title: 'parent 2',
+                key: '0-1',
+                children: [
+                  {
+                    title: 'parent 1-1',
+                    key: '0-1-0',
+                  },
+                ],
+              },
+            ]);
+          });
+
+          // 此处功能以后将要改造为新增请求时打开新的tab，并激活选中
+          // postman效果是选中了以后无法取消需要做到
+
+          setExpandedKeys([...expandedKeys,'0-1'])
+        }}
+      >
+        添加一个并展开
+      </Button>
     </div>
   );
 };
